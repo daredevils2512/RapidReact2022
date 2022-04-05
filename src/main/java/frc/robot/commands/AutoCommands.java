cc7ec058032655f3995351a09ff0a4bb0b17bbec
ike <ihschw241@isd709.org>
2022-04-04 19:57:17 -0500

    Fixed shoot again

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -13,7 +13,7 @@ public final class AutoCommands {
   private AutoCommands() {}
 
   public static Command autoShoot(Shooter shooter, Magazine magazine, Intake intake, double shootSpeed) {
-    return Commands.runIntake(intake, () -> 1.0)
+    return Commands.runMag(magazine, () -> 1.0)
     .withTimeout(3)
     .andThen(Commands.revShooter(shooter, shootSpeed))
     .alongWith(Commands.runMag(magazine, () -> 1.0));
